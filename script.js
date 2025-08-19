d2c957e4abde720dd2c0be21dfbdfafed299020a
mrmohan04 <gugulothmohan381@gmail.com>
2025-08-19 15:06:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-//your code here
 let draggedDiv = null;
 
 window.addEventListener('DOMContentLoaded', () => {
   const images = document.querySelectorAll('.image');
-
   images.forEach(image => {
+
     image.addEventListener('dragstart', (e) => {
       draggedDiv = image;
       image.classList.add('selected');
@@ -22,19 +21,15 @@ window.addEventListener('DOMContentLoaded', () => {
     image.addEventListener('drop', (e) => {
       e.preventDefault();
       if (draggedDiv && draggedDiv !== image) {
-        
         const draggedStyle = draggedDiv.style.backgroundImage;
         const droppedStyle = image.style.backgroundImage;
-
         draggedDiv.style.backgroundImage = droppedStyle;
         image.style.backgroundImage = draggedStyle;
       }
-      
       images.forEach(img => img.classList.remove('selected'));
     });
   });
 
- 
   images.forEach(image => {
     const computedStyle = getComputedStyle(image);
     image.style.backgroundImage = computedStyle.backgroundImage;
